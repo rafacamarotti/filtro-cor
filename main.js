2f28c257fd97582d330e91ebda9c3de9ea7e3b75
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2023-03-07 20:45:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload(){
+}
+function setup(){
+    canvas = creatCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+    tint_color = "";
+}
+function filter_tint(){
+    tint_color = document.getElementById("color_name").value;
+}
+function draw(){
+    image(video, 0, 0, 640, 480);
+    tint(tint_color);
+}
+function take_snapshot(){
+    save('cor.png');
+}
